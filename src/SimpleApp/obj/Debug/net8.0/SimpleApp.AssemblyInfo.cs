c15b4416c9f1c9eb287e60924ca1156b739fc45f
NumberThreeass <amirhanovapolina0@gmail.com>
2026-04-03 13:17:58 +0300

    feat: Добавлен персональный ассистент

--- src/SimpleApp/obj/Debug/net8.0/SimpleApp.AssemblyInfo.cs
+++ src/SimpleApp/obj/Debug/net8.0/SimpleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a7561adeea212a51bd74793bd40a760ae009d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/SimpleApp.Tests/obj/Debug/net8.0/SimpleApp.Tests.AssemblyInfo.cs
+++ tests/SimpleApp.Tests/obj/Debug/net8.0/SimpleApp.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a7561adeea212a51bd74793bd40a760ae009d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
